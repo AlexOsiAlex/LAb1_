d0f599e0a7cf598efebb9bcae96436b2fce94d8c
Adaladt <adaladt@yandex.ru>
2023-12-01 12:54:25 +0300

    Добавьте файлы проекта.

---
+++ LAb1_осипов/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAb1_осипов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
